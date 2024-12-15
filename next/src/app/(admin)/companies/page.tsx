ad59ae632b381e7b83280d1e387efb8e1cddd6f2
StasAstenenko <107870729+StasAstenenko@users.noreply.github.com>
2024-12-15 20:29:41 +0200

    import

--- next/src/app/(admin)/companies/page.tsx
+++ next/src/app/(admin)/companies/page.tsx
@@ -1,10 +1,10 @@
-import AddCompanyButton from "../components/add-company-button";
-import CompanyRow from "../components/company-row";
-import CompanyTable from "../components/company-table";
-import Header from "../components/header";
-import SearchInput from "../components/search-input";
-import { Status } from "../components/status-label";
-import Toolbar from "../components/toolbar";
+import AddCompanyButton from '@/app/components/add-company-button';
+import CompanyRow from '@/app/components/company-row';
+import CompanyTable from '@/app/components/company-table';
+import Header from '@/app/components/header';
+import SearchInput from '@/app/components/search-input';
+import { Status } from '@/app/components/status-label';
+import Toolbar from '@/app/components/toolbar';
 
 const Companies = () => {
   return (
@@ -17,11 +17,11 @@ const Companies = () => {
         <CompanyRow
           id={1}
           status={Status.Pending}
-          company={"Costco"}
-          country={"USA"}
-          category={"Products"}
+          company={'Costco'}
+          country={'USA'}
+          category={'Products'}
           promotion={true}
-          joinedDate={"02-19-2023"}
+          joinedDate={'02-19-2023'}
         />
       </CompanyTable>
     </>
